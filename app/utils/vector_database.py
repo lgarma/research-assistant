d0f1cf110a2e9738c74c9ce9a156c43c1d619114
lgarma <garmahds@gmail.com>
2023-09-21 00:57:18 -0600

    feat: added cache embedding and duplication check

--- app/utils/vector_database.py
+++ app/utils/vector_database.py
@@ -1,44 +1,53 @@
 """Vector database utilities."""
 import streamlit as st
-from langchain.embeddings.base import Embeddings
+from langchain.embeddings.cache import CacheBackedEmbeddings, _create_key_encoder
 from langchain.schema import Document
+from langchain.storage import LocalFileStore
 from langchain.vectorstores import Milvus
-from pymilvus import connections, utility
 
 
-def get_vector_db(
-    docs: list[Document], collection_name: str, embedding_function: Embeddings
-) -> Milvus:
-    """Get the Milvus vector database for the documents content.
+def connect_to_vector_db():
+    """Connect to pre-existing vector database.
 
-    If the collection already exists, it is loaded. If not, it is created.
-
-    Parameters:
-        docs (list[Document]):
-            List of documents to be loaded in the database.
-
-        collection_name (str):
-            Name of the collection to connect to.
-
-        embedding_function (Embeddings):
-            Embedding function to use to vectorize the documents.
+    Sets up a cached embedder to save the embeddings
+    The cached documents can be checked for duplicated documents.
     """
-    milvus_connection = {"alias": "default", "host": "localhost", "port": 19530}
-    connections.connect(**milvus_connection)
-    if utility.has_collection(collection_name):
-        st.write("Collection already exists. Loading collection...")
-        return Milvus(
-            collection_name=collection_name,
-            embedding_function=embedding_function,
-        )
-    else:
-        st.write(
-            f"Collection does not exist. Creating collection with name"
-            f" {collection_name}..."
-        )
-        return Milvus.from_documents(
-            documents=docs,
-            embedding=embedding_function,
-            connection_args=milvus_connection,
-            collection_name=collection_name,
-        )
+    state = st.session_state
+    state["cache"] = LocalFileStore(f"./cache/{state['collection_name']}")
+    state["cached_embedder"] = CacheBackedEmbeddings.from_bytes_store(
+        underlying_embeddings=state["embedding_model"],
+        document_embedding_cache=state["cache"],
+        namespace=state["collection_name"],
+    )
+    state["vector_db"] = Milvus(
+        collection_name=state["collection_name"],
+        embedding_function=state["cached_embedder"],
+    )
+
+
+def cache_documents_embeddings(docs: list[Document]):
+    """Get the embeddings for the documents and cache them.
+
+    Checks for duplicates before caching.
+    Returns list of documents that were not in the database.
+    """
+    state = st.session_state
+    new_docs = check_for_duplicates(docs)
+    if len(new_docs) > 0:
+        st.write(f"Encoding {len(new_docs)} new documents.")
+        state["cached_embedder"].embed_documents([doc.page_content for doc in new_docs])
+    return new_docs
+
+
+def check_for_duplicates(docs: list[Document]):
+    """Only return documents that where not in the database.
+
+    Extract the uuid of the documents. If the uuid is not in the database, return
+    the document.
+    """
+    state = st.session_state
+    # encoder used by langchain
+    key_encoder = _create_key_encoder(namespace=state["collection_name"])
+    keys = [key_encoder(doc.page_content) for doc in docs]
+    preexisting_keys = list(state["cache"].yield_keys())
+    return [doc for doc, key in zip(docs, keys) if key not in preexisting_keys]
