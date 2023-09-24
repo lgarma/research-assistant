a0b9879d5b8f19a76e25593f5c8e5f3d88165755
lgarma <garmahds@gmail.com>
2023-09-24 01:19:22 -0600

    feat: initialize llms cache store

--- app/utils/ui.py
+++ app/utils/ui.py
@@ -1,22 +1,22 @@
 """UI utilities for the app."""
 
+import langchain
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain.cache import SQLiteCache
 from langchain.embeddings import HuggingFaceEmbeddings
 from pymilvus import connections
+
+state = st.session_state
 
 
 def set_state_if_absent(key, value):
     """Set the state if it is absent."""
-    if key not in st.session_state:
-        st.session_state[key] = value
+    if key not in state:
+        state[key] = value
 
 
 def init_session_states():
     """Initialize the app."""
-    set_state_if_absent(
-        key="chat", value=ChatOpenAI(model="gpt-3.5-turbo", temperature=0.01)
-    )
     set_state_if_absent(key="app_state", value=None)
     set_state_if_absent(key="rows", value=2)
     set_state_if_absent(
@@ -28,14 +28,18 @@ def init_session_states():
 
 
 def start_app():
-    """Start the app."""
-    st.session_state.app_state = "initialized"
+    """Start the app.
+
+    Connect to Milvus and initialize the llm cache.
+    """
+    langchain.llm_cache = SQLiteCache(database_path=".app.db")
     milvus_connection = {"alias": "default", "host": "localhost", "port": 19530}
     connections.connect(**milvus_connection)
+    state.app_state = "initialized"
 
 
 def reset_app():
     """Reset the app."""
-    for key in st.session_state:
-        del st.session_state[key]
+    for key in state:
+        del state[key]
     init_session_states()
