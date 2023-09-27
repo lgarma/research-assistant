38e805e0b140f91e4dc41978ca5d62425dd6d945
lgarma <garmahds@gmail.com>
2023-09-27 02:25:57 -0600

    feat: create visualization of documents and evolution over time of topics

--- app/pages/02_💬_Topic_models.py
+++ app/pages/02_💬_Topic_models.py
@@ -1,30 +1,43 @@
 """Topic models page."""
+import os
 
 import streamlit as st
 from app.utils.topic_model import TopicModel
-from app.utils.vector_database import connect_to_vector_db
+from app.utils.ui import choose_collection
+from app.utils.vector_database import display_vector_db_info
 from pymilvus import utility
 
-st.title("Topic models")
 state = st.session_state
-
-st.button("Do nothing button")
-st.write(state)
+st.title("Topic models")
+st.sidebar.button("Do nothing button")
 
 collections = utility.list_collections()
-collections = sorted([c.replace("_", " ").title() for c in collections])
-collection_name = st.selectbox("Select a research collection", options=collections)
+choose_collection(collections=collections, use_sidebar=False)
 
-collection_name = collection_name.replace(" ", "_").lower()
-state["collection_name"] = collection_name
+display_vector_db_info()
 
-if st.button("Create topic model"):
-    connect_to_vector_db()
-
+if st.checkbox("Create topic model for this collection"):
     topic_model = TopicModel()
-    st.write(f"Documents: {len(topic_model.documents)}")
 
-    topic_model.fit_model()
-    document_viz = topic_model.visualize_documents()
+    if os.path.exists(f"./cache/{state['collection_name']}/topic_model_docs"):
+        st.write("Loading topic model from cache...")
+        topic_model.load_model()
 
-    st.plotly_chart(document_viz)
+    if st.button("Fit topic model"):
+        topic_model.fit_model()
+
+    topics = topic_model.topic_model.get_topics()
+    st.write("### Topics")
+    st.write("The following topics were found in the collection:")
+
+    st.dataframe(topic_model.topics_info())
+
+    if st.checkbox("Show documents visualization"):
+        document_viz = topic_model.visualize_documents()
+        if st.checkbox("Hide legends", value=True):
+            document_viz.update_layout(showlegend=False)
+        st.plotly_chart(document_viz, use_container_width=True)
+
+    if st.checkbox("Show topics over time"):
+        topics_over_time_viz = topic_model.visualize_over_time()
+        st.plotly_chart(topics_over_time_viz, use_container_width=True)
